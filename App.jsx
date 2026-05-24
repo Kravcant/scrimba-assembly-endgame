ba80e004e542382712514f8102e47aa1dc10faa8
anthonykravchishin <tonykravc53@gmail.com>
2026-05-23 20:25:43 -0700

    Disable keyboard when the game is over

--- App.jsx
+++ App.jsx
@@ -66,6 +66,7 @@ export default function AssemblyEndgame() {
             <button
                 className={className}
                 key={letter}
+                disabled={isGameOver}
                 onClick={() => addGuessedLetter(letter)}
             >
                 {letter.toUpperCase()}
