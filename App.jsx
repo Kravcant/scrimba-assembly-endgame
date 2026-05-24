86edee72443b7f4046b21be1cc7ed76f431da347
anthonykravchishin <tonykravc53@gmail.com>
2026-05-23 17:07:34 -0700

    Status section

--- App.jsx
+++ App.jsx
@@ -1,12 +1,5 @@
 import React from "react"
 
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
- */
-
 export default function AssemblyEndgame() {
     return (
         <main>
@@ -15,6 +8,10 @@ export default function AssemblyEndgame() {
                 <p>Guess the word within 8 attempts to keep the 
                 programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
